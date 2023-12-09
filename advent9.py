84f8276f4ede5cae6c4436b8a21124590caa8972
katstasaph <katherinestasaph@gmail.com>
2023-12-09 17:03:37 -0500

    removed extraneous code (we don't need to actually calculate the diffs, we know it'll work per the problem)

--- advent9.py
+++ advent9.py
@@ -1,27 +1,17 @@
-from functools import reduce
-from numpy import diff
 import sympy
-
-def finite_diffs(sequence):
-    curr = [int(n) for n in sequence]
-    diffs = [curr]
-    while reduce(lambda a, b: a + b, curr) / len(curr) != curr[0]:
-        curr = list(diff(curr))
-        diffs.append(curr)
-    return diffs
 
 with open("advent9.txt", "r") as f:
     sequences = [line.split(" ") for line in f.read().splitlines()]
     sum = 0
     sum2 = 0
     for sequence in sequences:
-        table = finite_diffs(sequence)[0]
+        nums = [int(n) for n in sequence]
         # If we can eventually derive common differences from a sequence, there exists some polynomial that produces it
         # more info:
         # www.cgsd.org/site/handlers/filedownload.ashx?moduleinstanceid=38&dataid=183&FileName=772-SMP-SEAA-C11L07.pdf
-        poly = sympy.polys.specialpolys.interpolating_poly(len(table), sympy.symbols("x"),
-                                                           [idx + 1 for (idx, _) in enumerate(table)], table)
-        sum += poly.subs("x", (len(table)) + 1)
+        poly = sympy.polys.specialpolys.interpolating_poly(len(nums), sympy.symbols("x"),
+                                                           [idx + 1 for (idx, _) in enumerate(nums)], nums)
+        sum += poly.subs("x", (len(nums)) + 1)
         sum2 += poly.subs("x", 0)
     print(f"part 1: {sum}")
     print(f"part 2: {sum2}")
